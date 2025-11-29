1d234dac7196736b14391ba0e44bf6270be1b72d
Majeeee <Majid.Tajik@stud.sti.se>
2025-11-29 23:23:11 +0100

    Uppdate repository and service layers for SCB movements

--- src/main/java/se/maje/scb_movements_backend/service/MovementRepository.java
+++ src/main/java/se/maje/scb_movements_backend/service/MovementRepository.java
@@ -1,6 +1,9 @@
 package se.maje.scb_movements_backend.service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import se.maje.scb_movements_backend.model.MovementRecord;
 
-public interface MovementRepository extends JpaRepository<MovementRecord, Long> {}
+public interface MovementRepository extends JpaRepository<MovementRecord, Long>,
+        JpaSpecificationExecutor<MovementRecord> {
+}
